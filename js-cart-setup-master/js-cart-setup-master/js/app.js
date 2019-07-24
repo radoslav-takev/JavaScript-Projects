4a84425998c40bf69df7027f458665d9eb442b0b
radoslav-takev <takev.radoslav@gmail.com>
2019-07-24 15:03:35 +0300

    Shopping Cart
    initial commit

---
+++ js-cart-setup-master/js-cart-setup-master/js/app.js
@@ -0,0 +1,51 @@
+//show cart
+
+(function(){
+const cartInfo = document.getElementById('cart-info');
+const cart = document.getElementById('cart');
+
+cartInfo.addEventListener('click', function(){
+    cart.classList.toggle('show-cart');
+});
+})();
+
+// add items to the cart
+
+(function(){
+    const cartBtn = document.querySelectorAll('.store-item-icon');
+
+    cartBtn.forEach(function(btn){
+        btn.addEventListener('click',function(event){
+            //console.log(event.target);
+    if(event.target.parentElement.classList.contains('store-item-icon')){
+        let fullPath = event.target.parentElement.previousElementSibling.src;
+
+        let pos = fullPath.indexOf('img') + 3;
+
+        let partPath = fullPath.slice(pos);
+        
+        const item = {};
+        item.img = `img-cart${partPath}`;
+
+        let name = event.target.parentElement.parentElement.nextElementSibling
+            .children[0].children[0].textContent;
+        item.name = name;
+
+        let price = event.target.parentElement.parentElement.nextElementSibling
+        .children[1].children[1].textContent;
+
+        console.log(price);
+        console.log(item);
+        
+        
+
+
+
+    }
+
+        });
+    });
+
+
+
+})();
