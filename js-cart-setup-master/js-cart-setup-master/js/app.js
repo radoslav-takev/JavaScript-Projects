9fb2f5bbcf69278d105efe35b2d1ab5a3397c2fc
radoslav-takev <takev.radoslav@gmail.com>
2019-07-24 15:58:51 +0300

    Shopping Cart
    Only adding items to the shopping cart list and calculating the total amount.

--- js-cart-setup-master/js-cart-setup-master/js/app.js
+++ js-cart-setup-master/js-cart-setup-master/js/app.js
@@ -32,20 +32,71 @@ cartInfo.addEventListener('click', function(){
         item.name = name;
 
         let price = event.target.parentElement.parentElement.nextElementSibling
-        .children[1].children[1].textContent;
+        .children[0].children[1].textContent;
+
+        let finalPrice = price.slice(1).trim();
+
+        item.price = finalPrice;
 
         console.log(price);
+        console.log(finalPrice);
         console.log(item);
-        
-        
 
+        const cartItem = document.createElement("div");
+            cartItem.classList.add(
+                'cart-item',
+                'd-flex',
+                'justify-content-between',
+                'text-capitalize',
+                'my-3'
+            );
+            
+            cartItem.innerHTML=`
+            <img src="${item.img}" class="img-fluid rounded-circle" id="item-img" alt="">
+              <div class="item-text">
+  
+                <p id="cart-item-title" class="font-weight-bold mb-0">${item.name}</p>
+                <span>$</span>
+                <span id="cart-item-price" class="cart-item-price" class="mb-0">${item.price}</span>
+              </div>
+              <a href="#" id='cart-item-remove' class="cart-item-remove">
+                <i class="fas fa-trash"></i>
+              </a>
+            </div>
+            `;
 
+            //select cart
 
-    }
+            const cart = document.getElementById('cart');
+            const total = document.querySelector('.cart-total-container');
+
+            cart.insertBefore(cartItem,total);
+            alert('item added to the cart');
+            showTotals();
+            }
 
         });
     });
 
+    //show totals
+    function showTotals(){
 
+        const total = [];
+        const items = document.querySelectorAll('.cart-item-price');
+
+        items.forEach(function(item){
+            total.push(parseFloat(item.textContent));
+        });
+        //console.log(total);
+        const totalMoney = total.reduce(function(total, item){
+            total += item;
+            return total;
+        },0);
+        const finalMoney = totalMoney.toFixed(2);
+        
+        document.getElementById('cart-total').textContent = finalMoney;
+        //document.getElementById('.item-total').textContent = finalMoney;
+        document.getElementById('item-count').textContent = total.length;
+    }   
 
 })();
